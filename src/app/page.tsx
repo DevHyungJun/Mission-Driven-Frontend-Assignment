444a13c83759c45b10c62ce7d97813a70518d450
jun <znznun@gmail.com>
2025-11-13 21:34:10 +0900

    feat(textarea): textarea 컴포넌트 작업 기초 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { Textarea } from "@/components";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-2">
       <h1>메인 페이지</h1>
-      <Textarea placeholder="텍스트를 입력해주세요" />
+      <Textarea
+        placeholder="텍스트를 입력해주세요"
+        errorMessage="에러 메시지"
+      />
     </div>
   );
 }
--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,18 +1,29 @@
 interface TextareaProps {
   placeholder: string;
+  errorMessage?: string;
+  currentMessageLength?: number;
 }
 
-const Textarea = ({ placeholder }: TextareaProps) => {
+const Textarea = ({
+  placeholder,
+  errorMessage,
+  currentMessageLength = 0,
+}: TextareaProps) => {
   return (
-    <div className="w-full rounded-lg border border-gray-300 focus-within:border-[#03C124]">
-      <textarea
-        className="resize-none w-full min-h-[80px] leading-[130%] placeholder:text-[#8F8F8F] focus:outline-none px-[16px] py-[12px] text-[#121212]"
-        placeholder={placeholder}
-      />
-      <p className="min-h-[38px] text-right text-[#8F8F8F] text-sm">
-        0/80자 (최소 8자)
-      </p>
-    </div>
+    <>
+      <div className="px-[16px] w-full rounded-lg border border-gray-300 focus-within:border-[#03C124]">
+        <textarea
+          className="resize-none w-full min-h-[80px] leading-[130%] placeholder:text-[#8F8F8F] focus:outline-none py-[12px] text-[#121212]"
+          placeholder={placeholder}
+        />
+        <label className="flex justify-end items-center min-h-[38px] text-[#8F8F8F] text-sm">
+          {currentMessageLength}/80자 (최소 8자)
+        </label>
+      </div>
+      {errorMessage && (
+        <span className="mt-1 block text-[#E82929]">{errorMessage}</span>
+      )}
+    </>
   );
 };
 
