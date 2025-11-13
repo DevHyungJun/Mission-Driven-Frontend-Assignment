51307f9b17ef6c2d290b5b7e72a52d307869130f
jun <znznun@gmail.com>
2025-11-13 23:13:09 +0900

    style: 반응형 브레이크 포인트 기입

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    screens: {
+      pc: "1100px",
+    },
     extend: {
       fontFamily: { sans: ["var(--font-pretendard)", "Inter", "sans-serif"] },
     },
