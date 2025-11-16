d866b7d34efa2ece3a2df460ffe64fe3bcd80d0e
jun <znznun@gmail.com>
2025-11-17 03:02:11 +0900

    delete(Icon): 폴더 삭제

--- src/components/icon/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Plus } from "@/app/_assets/image-plus.svg";
-export { default as ChevronLeft } from "@/app/_assets/chevron-left.svg";
-export { default as ChevronRight } from "@/app/_assets/chevron-right.svg";
-export { default as X } from "@/app/_assets/x.svg";
