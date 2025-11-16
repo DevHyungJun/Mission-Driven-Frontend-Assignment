991706311b74ec50f42b46a9941fa4a27fc025ef
jun <znznun@gmail.com>
2025-11-17 02:46:47 +0900

    import: Icon import문 재수정

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,4 +1,3 @@
-export { default } from "./Icon";
 export { default as Plus } from "@/app/_assets/image-plus.svg";
 export { default as ChevronLeft } from "@/app/_assets/chevron-left.svg";
 export { default as ChevronRight } from "@/app/_assets/chevron-right.svg";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Textarea } from "./Textarea/Textarea";
-export { default as Icon } from "./Icon";
+export { default as Icon } from "./Icon/Icon";
 export { default as Button } from "./Button/Button";
 export { default as Header } from "./Header/Header";
 export { default as Calender } from "./Calender/Calender";
