17877df82d1a0045440751a9d07ee392dceb0f0c
jun <znznun@gmail.com>
2025-11-17 02:42:50 +0900

    fix(import): Icon 배럴패턴 빌드 에러 해결

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,3 +1,4 @@
+export { default } from "./Icon";
 export { default as Plus } from "@/app/_assets/image-plus.svg";
 export { default as ChevronLeft } from "@/app/_assets/chevron-left.svg";
 export { default as ChevronRight } from "@/app/_assets/chevron-right.svg";
