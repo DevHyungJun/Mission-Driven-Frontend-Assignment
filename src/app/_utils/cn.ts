859b995635805519553fc07b04798c490d9f2485
jun <znznun@gmail.com>
2025-11-17 02:19:27 +0900

    docs(cn): 주석

--- src/app/_utils/cn.ts
+++ src/app/_utils/cn.ts
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// clsx와 tailwind-merge를 사용하여 클래스 병합
 export const cn = (...classes: (string | undefined | boolean)[]) => {
   return twMerge(clsx(...classes));
 };
