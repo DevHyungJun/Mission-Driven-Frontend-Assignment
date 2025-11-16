734805b9f31d8f034458098524b10fca005e1075
jun <znznun@gmail.com>
2025-11-17 01:46:31 +0900

    docs: getVariantStyle 주석

--- src/components/Button/utils/getVariantStyles.ts
+++ src/components/Button/utils/getVariantStyles.ts
@@ -2,6 +2,7 @@ import { cn } from "@/app/_utils/cn";
 import { COLOR_STYLES, VARIANT_STYLES } from "../constants/STYLE_CONSTANTS";
 import { Color, Variant } from "../types/ButtonTypes";
 
+// variant와 color를 받아서 스타일을 반환하는 함수
 const getVariantStyles = (variant: Variant, color: Color) => {
   if (variant === "default") {
     return cn(VARIANT_STYLES.default, COLOR_STYLES[color], "text-white");
